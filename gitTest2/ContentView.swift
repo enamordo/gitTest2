901c4337e402b7ed97fc413b3b686c267d6c84b0
Atsunobu Yuge <yugechan666@gmail.com>
2021-11-03 23:07:55 +0900

    Initial Commit

---
+++ gitTest2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitTest2
+//
+//  Created by 弓削　敦信 on 2021/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gitTest2/gitTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  gitTest2App.swift
+//  gitTest2
+//
+//  Created by 弓削　敦信 on 2021/11/03.
+//
+
+import SwiftUI
+
+@main
+struct gitTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
